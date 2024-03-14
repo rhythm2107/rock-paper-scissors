5595c25530fa3ed1a7ecbd230f6573d6a34b19f1
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-03-15 00:23:08 +0100

    Remove pseudocode

--- logic.js
+++ logic.js
@@ -63,14 +63,3 @@ function playGame() {
     }
 
 }
-
-    // dictionary of what beats what -> {rock: scissors, scissors: paper, paper: rock}
-    // testing with playerchoice of rock
-    // if dictionary[p_choice] (returns scissors) === computerchoice (lets say scissors) -> player wins because picked rock and computer picked scissors
-    // if player picks scissors then draw above pops
-    // if player picks paper then he loses so essentially just do else lose
-
-    //PSEUDOCODE
-    // DICT = {rock: scissors, scissors: paper, paper: rock}
-    // IF DICT[p_choice] === c_choice -> return player win
-    // ELSE -> return player lose
