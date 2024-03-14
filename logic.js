e0172aebdf9d8116be861fdcd23f1bc82522bd06
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-03-14 21:48:13 +0100

    Convert pseudocode into code

--- logic.js
+++ logic.js
@@ -11,10 +11,27 @@ function playRound(playerSelection, computerSelection) {
     
     let p_choice = playerSelection.toLowerCase();
     let c_choice = computerSelection;
+    const winDictionary = {
+        "rock": "scissors",
+        "scissors": "paper",
+        "paper": "rock",
+    }
+    
+    let print_p_choice = p_choice.charAt(0).toUpperCase() + p_choice.slice(1)
+    let print_c_choice = c_choice.charAt(0).toUpperCase() + c_choice.slice(1)
 
     if (p_choice === c_choice) {
-        console.log('Draw!')
+        console.log('It\'s a draw!')
+    } else if (winDictionary[p_choice] === c_choice) {
+        console.log(`You won! ${print_p_choice} beats ${print_c_choice}!`)
+    } else {
+        console.log(`You lose! ${print_c_choice} beats ${print_p_choice}!`)
     }
+
+    console.log(winDictionary['rock'])
+    console.log(winDictionary['scissors'])
+    console.log(winDictionary['paper'])
+
     // dictionary of what beats what -> {rock: scissors, scissors: paper, paper: rock}
     // testing with playerchoice of rock
     // if dictionary[p_choice] (returns scissors) === computerchoice (lets say scissors) -> player wins because picked rock and computer picked scissors
