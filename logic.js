f00138acb41caff9e6bf1871ceb9a7c5b4ccbe25
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-06-22 00:02:56 +0200

    Update logic.js with a fully working eventListener based logic. Add divs for displaying results and messages on page.

--- logic.js
+++ logic.js
@@ -1,45 +1,51 @@
-function getPrompt() {
-    return prompt("Choose rock, paper or scissors!")
-}
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
     let randomIndex = Math.floor(Math.random() * choices.length)
     return choices[randomIndex]
 }
+function startGame() {
+    var info = document.getElementById('start-info');
+    var buttonContainer = document.getElementById('button-container');
 
-function playRound(playerSelection, computerSelection) {
-    
-    let p_choice = playerSelection.toLowerCase();
-    let c_choice = computerSelection;
+    // Hide the initial game info and show the buttons
+    info.style.display = 'none';
+    buttonContainer.style.display = 'flex';
+}
+
+function playGame(choice) {
+    console.log(choice)
+}
+
+let p_score = 0;
+let c_score = 0;
+let roundsPlayed = 0;
+
+document.querySelector('.fire').addEventListener('click', () => playGame('rock'));
+document.querySelector('.water').addEventListener('click', () => playGame('paper'));
+document.querySelector('.grass').addEventListener('click', () => playGame('scissors'));
+document.querySelector('.start-button').addEventListener('click', () => startGame())
+
+
+function playRound(playerChoice, computerChoice) {
     const winDictionary = {
         "rock": "scissors",
         "scissors": "paper",
         "paper": "rock",
     }
-    
-    let print_p_choice = p_choice.charAt(0).toUpperCase() + p_choice.slice(1)
-    let print_c_choice = c_choice.charAt(0).toUpperCase() + c_choice.slice(1)
 
-    if (p_choice === c_choice) {
-        console.log(`It\'s a draw! Computer picked ${print_c_choice}!`)
+    if (playerChoice === computerChoice) {
         return 'draw'
-    } else if (winDictionary[p_choice] === c_choice) {
-        console.log(`You won! ${print_p_choice} beats ${print_c_choice}!`)
+    } else if (winDictionary[playerChoice] === computerChoice) {
         return 'win'
     } else {
-        console.log(`You lose! ${print_c_choice} beats ${print_p_choice}!`)
         return 'lose'
     }
 }
-// console.log(playRound(getPrompt(), getComputerChoice()))
 
 function playGame(choice) {
-    p_score = 0
-    c_score = 0
-
-    for (let i = 1; i <= 5; i++) {
+    if (roundsPlayed < 5) {
         let result = playRound(choice, getComputerChoice())
+
         if (result === 'draw') {
             console.log(`A tie! The score is ${p_score} - ${c_score}`)
         } else if (result === 'win') {
@@ -49,32 +55,19 @@ function playGame(choice) {
             c_score++
             console.log(`You lost this round! The score is ${p_score} - ${c_score}`)
         }
+        roundsPlayed++;
+        if (roundsPlayed === 5) {
+            evaluateFinalScore();
+        }
     }
-    
+}
+
+function evaluateFinalScore() {
     if (p_score > c_score) {
-        console.log(`Congratulations! You won with a score of ${p_score} - ${c_score}`)
+        console.log(`Congratulations! You won with a score of ${p_score} - ${c_score}`);
     } else if (p_score < c_score) {
-        console.log(`Better luck next time! You lost with a score of ${p_score} - ${c_score}`)
+        console.log(`Better luck next time! You lost with a score of ${p_score} - ${c_score}`);
     } else {
-        console.log(`What a tough match! You tied with a score of ${p_score} - ${c_score}`)
+        console.log(`What a tough match! You tied with a score of ${p_score} - ${c_score}`);
     }
-
 }
-
-function startGame() {
-    var info = document.getElementById('start-info');
-    var buttonContainer = document.getElementById('button-container');
-
-    // Hide the initial game info and show the buttons
-    info.style.display = 'none';
-    buttonContainer.style.display = 'flex';
-    playGame()
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.querySelector('.flex-button');
-    
-    button.addEventListener('click', function() {
-        playRound('rock')
-    });
-});
